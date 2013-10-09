1a21db9c3768218c8639d129af55491824585ff6
danielekbom <daniel5.2@hotmail.com>
2013-10-09 13:44:40 +0200

    daniel

--- enemy1.py
+++ enemy1.py
@@ -5,6 +5,8 @@ import random
 class Enemy1:
 	def __init__(self):
 		self.ball = Image(Point(1400,random.randint(20,700)),"images/enemy1.gif")
+		pictures = [1,2,3,4,5,6,7,8,9]
+		currentImage = self.
 	
 	def draw(self,window):
 		self.ball.draw(window)
@@ -19,3 +21,9 @@ class Enemy1:
 		self.ball.undraw()
 		eliminated = Image(self.getCenter(),"images/enemy1_eliminated.gif")
 		eliminated.draw(window)
+		
+	def nextImage(self):
+		self.ball.undraw()
+		self.ball = Image(self.getCenter(),"images/enemy1/" + x +  ".gif")
+		eliminated.draw(window)
+		
