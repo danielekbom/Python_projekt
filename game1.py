01a80990d9c97bebdcbddb69b329633451e202a9
danielekbom <daniel5.2@hotmail.com>
2013-10-13 21:32:03 +0200

    skapat enemy10

--- game1.py
+++ game1.py
@@ -10,6 +10,7 @@ import enemy6
 import enemy7
 import enemy8
 import enemy9
+import enemy10
 import boss1
 import boss2
 
@@ -52,7 +53,7 @@ def main():
 			break
 		randNumber = random.randint(1,100)
 		if(randNumber == 50):
-			enemyTypes = [enemy2.Enemy2,enemy3.Enemy3,enemy4.Enemy4,boss1.Boss1,enemy5.Enemy5,enemy6.Enemy6,enemy7.Enemy7,boss2.Boss2,enemy8.Enemy8,enemy9.Enemy9]
+			enemyTypes = [enemy2.Enemy2,enemy3.Enemy3,enemy4.Enemy4,boss1.Boss1,enemy5.Enemy5,enemy6.Enemy6,enemy7.Enemy7,boss2.Boss2,enemy8.Enemy8,enemy9.Enemy9,enemy10.Enemy10]
 			enemies.append(random.choice(enemyTypes)())
 			lastCircle = len(enemies) - 1
 			enemies[lastCircle].draw(win)
