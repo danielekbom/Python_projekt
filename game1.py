92448ac37611bfc6f78df83742478322b92173e1
danielekbom <daniel5.2@hotmail.com>
2013-10-13 02:39:55 +0200

    skapat boss2

--- game1.py
+++ game1.py
@@ -9,6 +9,7 @@ import enemy5
 import enemy6
 import enemy7
 import boss1
+import boss2
 
 def main():
    
@@ -49,7 +50,7 @@ def main():
 			break
 		randNumber = random.randint(1,100)
 		if(randNumber == 50):
-			enemyTypes = [enemy1.Enemy1,enemy2.Enemy2,enemy3.Enemy3,enemy4.Enemy4,boss1.Boss1,enemy5.Enemy5,enemy6.Enemy6,enemy7.Enemy7]
+			enemyTypes = [enemy1.Enemy1,enemy2.Enemy2,enemy3.Enemy3,enemy4.Enemy4,boss1.Boss1,enemy5.Enemy5,enemy6.Enemy6,enemy7.Enemy7,boss2.Boss2]
 			enemies.append(random.choice(enemyTypes)())
 			lastCircle = len(enemies) - 1
 			enemies[lastCircle].draw(win)
