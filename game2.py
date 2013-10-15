cebeb1ff2269a6ab4a1e9ffaa052d9e233062f88
danielekbom <daniel5.2@hotmail.com>
2013-10-16 00:29:51 +0200

    Hämtar clickkordinater när man trycker.
    Samt testar funktionen genom att ändra storlek på currentScore.

--- game2.py
+++ game2.py
@@ -81,6 +81,9 @@ class Game(GraphWin):
 		while self.gameRunning:
 			mouseClick = self.checkMouse()
 			if mouseClick:
+				click_x = mouseClick.getX()
+				click_y = mouseClick.getY()
+				currentScore.setSize(36)
 				pass
 				
 			for enemy in enemies:
