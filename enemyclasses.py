58ce80363564849ca1d657478800f46b2aad1d3c
Alexander Andersson <alexander@samproduktionab.se>
2013-10-15 15:38:48 +0200

    Skrivit om enemyclasses

--- enemyclasses.py
+++ enemyclasses.py
@@ -1,29 +1,63 @@
 from enemybaseclass import *
 
-class enemy1(Enemies):
-    level=1
-    '''Enemy 1'''
+# Normal enemies inherits the Enemy class
 
-class enemy2(Enemies):
+class enemy2(Enemy):
+    '''Enemy 2'''
+    id=2
     level=1
-    '''Enemy 1'''
 
-class enemy3(Enemies):
+class enemy3(Enemy):
+    '''Enemy 3'''
+    id=3
     level=1
-    '''Enemy 1'''
 
-class enemy4(Enemies):
+class enemy4(Enemy):
+    '''Enemy 4'''
+    id=4
     level=2
-    '''Enemy 1'''
+    hp=2
 
-class enemy5(Enemies):
+class enemy5(Enemy):
+    '''Enemy 5'''
+    id=5
     level=2
-    '''Enemy 1'''
+    hp=4
 
-class enemy6(Enemies):
+class enemy6(Enemy):
+    '''Enemy 6'''
+    id=6
+    level=2
+    hp=5
+
+class enemy7(Enemy):
+    '''Enemy 7'''
+    id=7
     level=3
-    '''Enemy 1'''
+    hp=7
+
+class enemy8(Enemy):
+    '''Enemy 8'''
+    id=8
+    level=3
+    hp=8
+
+# Bosses inherits the Boss class 
 
 class boss1(Boss):
+    '''Boss 1'''
+    id=1
     level=1
-    '''Boss''' 
+    hp=10
+
+class boss2(Boss):
+    '''Boss 2'''
+    id=2
+    level=2
+    hp=25
+
+class boss3(Boss):
+    '''Boss 3'''
+    id=3
+    level=3
+    hp=50
